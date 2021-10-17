f4563534f576ab7b1e9c50a175db08d8b81db427
jkat11 <87955392+jkat11@users.noreply.github.com>
2021-10-18 05:49:21 +1100

    Add files via upload

---
+++ js/vega_lite_vis.js
@@ -0,0 +1,4 @@
+var spec3 = "average.json";
+vegaEmbed('#bar_chart', spec3).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
